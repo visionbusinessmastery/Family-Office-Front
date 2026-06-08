1259c3a563a01ab39738e81760ddc291b92d327c
visionbusinessmastery <visionbusinessmastery@gmail.com>
2026-06-08 10:01:31 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Playfair_Display, Quicksand } from "next/font/google";
 import CookieConsentBanner from "@/components/privacy/CookieConsentBanner";
+import EthanFloatingAdvisor from "@/components/dashboard/EthanFloatingAdvisor";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -50,6 +51,7 @@ export default function RootLayout({
         }}
       >
         {children}
+        <EthanFloatingAdvisor />
         <CookieConsentBanner />
       </body>
     </html>
